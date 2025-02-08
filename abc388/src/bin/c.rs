02a5e02ef736419e8515b90d86e559045988a48e
福原有信 <afukuhara@abc.com>
2025-02-08 16:31:24 +0900

    finish by reading example

--- abc388/src/bin/c.rs
+++ abc388/src/bin/c.rs
@@ -10,17 +10,41 @@ use proconio::{
 #[allow(unused_imports)]
 use std::cmp::{max, min};
 #[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    input! {
+        mochi: [usize; n]
+    };
+
+    // my original code
+    // let mut sorted_mochi: BTreeMap<&usize, usize> = BTreeMap::new();
+    // let mut result = 0;
+
+    // mochi.iter().for_each(|m| {
+    //     let max = m / 2;
+    //     let valid_mochi_count = sorted_mochi.range(0..=max).map(|(_, v)| v).sum::<usize>();
+    //     result += valid_mochi_count;
+    //     *sorted_mochi.entry(m).or_default() += 1;
+    // });
+
+    let mut result = 0;
+    let mut j = 0; // a / 2 より大きい最初の要素（なければ最後の次）のインデックス
+
+    for &a in &mochi {
+        // Advance j until A[j] is greater than a/2 (i.e. while A[j]*2 <= a).
+        while j < n && mochi[j] * 2 <= a {
+            j += 1;
+        }
+        result += j;
+    }
+
+    println!("{}", result);
 }
