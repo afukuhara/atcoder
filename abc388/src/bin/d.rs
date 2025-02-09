4b2bf3c5e8fd59014f0138cfbd2d9c96c04738bb
福原有信 <afukuhara@abc.com>
2025-02-09 15:37:16 +0900

    finish by reading example

--- abc388/src/bin/d.rs
+++ abc388/src/bin/d.rs
@@ -17,10 +17,49 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    input! {
+        mut aliens: [usize; n]
+    };
+
+    // My code
+    // let mut carrie_over = 0;
+    // for i in 0..n {
+    //     let add = min(i, carrie_over);
+    //     aliens[i] += add;
+    //     carrie_over -= add;
+
+    //     println!(
+    //         "<Middle> i: {:2}, carrie_over: {:2}, aliens: {:?}",
+    //         i, carrie_over, aliens
+    //     );
+
+    //     let give = min(aliens[i], n - i - 1);
+    //     aliens[i] -= give;
+    //     carrie_over += give;
+
+    //     println!(
+    //         "<After>  i: {:2}, carrie_over: {:2}, aliens: {:?}",
+    //         i, carrie_over, aliens
+    //     );
+    //     println!("--------------------------------");
+    // }
+
+    let mut result = vec![0; n];
+    let mut diff: Vec<isize> = vec![0; n + 1];
+    for i in 0..n {
+        if i != 0 {
+            result[i] = result[i - 1] + diff[i];
+            aliens[i] += result[i] as usize;
+        }
+
+        let cnt = min(n - i - 1, aliens[i]);
+        aliens[i] -= cnt;
+        diff[i + 1] += 1;
+        diff[min(n, i + cnt + 1)] -= 1;
+    }
+
+    println!("{}", aliens.iter().join(" "));
 }
