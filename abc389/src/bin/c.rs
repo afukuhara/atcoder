3a77db464493834001129c9f0cc23a4a3e125afa
福原有信 <afukuhara@abc.com>
2025-02-15 14:38:01 +0900

    finish problem C

--- abc389/src/bin/c.rs
+++ abc389/src/bin/c.rs
@@ -1,26 +1,57 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        q: usize
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut queries = vec![];
+    for _ in 0..q {
+        input! {
+            a: usize
+        };
+
+        if a == 1 || a == 3 {
+            input! {
+                b: usize
+            };
+            queries.push((a, b));
+        } else {
+            queries.push((a, 0));
+        }
+    }
+
+    let mut sum_at_index = vec![];
+    let mut offset = 0;
+    for (i, j) in &queries {
+        match i {
+            1 => {
+                let new_sum = match sum_at_index.last() {
+                    Some(sum) => sum + *j,
+                    None => *j,
+                };
+                sum_at_index.push(new_sum);
+            }
+            2 => {
+                offset += 1;
+            }
+            3 => {
+                let count = if *j > 0 { *j - 1 } else { 0 };
+
+                if count == 0 {
+                    println!("0");
+                    continue;
+                }
+
+                let sum = if offset == 0 {
+                    sum_at_index[offset + count - 1]
+                } else {
+                    sum_at_index[offset + count - 1] - sum_at_index[offset - 1]
+                };
+                println!("{}", sum);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
