0befef492c4dbf6985bda6ef8b710f64ec87b56a
福原有信 <afukuhara@abc.com>
2025-02-22 15:55:09 +0900

    finish problem C

--- abc390/src/bin/c.rs
+++ abc390/src/bin/c.rs
@@ -1,23 +1,10 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
+use proconio::{fastout, input, marker::Chars};
 use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, _w: usize,
+        h: usize, w: usize,
     };
 
     let mut lines = vec![];
@@ -30,19 +17,8 @@ fn main() {
 
     let mut max_x = 0;
     let mut max_y = 0;
-    let mut min_x = 1001;
-    let mut min_y = 1001;
-
-    // for i in 0..h {
-    //     for j in 0..w {
-    //         if lines[i][j] == '#' {
-    //             max_x = max(max_x, i);
-    //             max_y = max(max_y, j);
-    //             min_x = min(min_x, i);
-    //             min_y = min(min_y, j);
-    //         }
-    //     }
-    // }
+    let mut min_x = w;
+    let mut min_y = h;
 
     for (i, line) in lines.iter().enumerate() {
         for (j, c) in line.iter().enumerate() {
@@ -55,12 +31,11 @@ fn main() {
         }
     }
 
-    // println!("max: {} {} min: {} {}", max_x, max_y, min_x, min_y);
     let mut ans = "Yes";
 
-    for i in min_x..max_x {
-        for j in min_y..max_y {
-            if lines[i][j] == '.' {
+    for line in lines.iter().take(max_x + 1).skip(min_x) {
+        for cell in line.iter().take(max_y + 1).skip(min_y) {
+            if *cell == '.' {
                 ans = "No";
                 break;
             }
