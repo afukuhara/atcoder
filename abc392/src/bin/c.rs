1730862a8ae4228aa6057974b136959bd82995a1
福原有信 <afukuhara@abc.com>
2025-03-08 16:13:05 +0900

    finish problem C

--- abc392/src/bin/c.rs
+++ abc392/src/bin/c.rs
@@ -1,26 +1,54 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use itertools::Itertools;
+use proconio::{fastout, input};
+
+struct Person {
+    watching_person: usize,
+    bibs: usize,
+}
+
+impl Person {
+    fn new(watching_person: usize, bibs: usize) -> Self {
+        Self {
+            watching_person,
+            bibs,
+        }
+    }
+
+    fn watching_person(&self) -> usize {
+        self.watching_person
+    }
+
+    fn bibs(&self) -> usize {
+        self.bibs
+    }
+}
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut people = vec![];
+    for _ in 0..n {
+        input! {
+            p: usize,
+        }
+
+        people.push(Person::new(p, 0));
+    }
+
+    let mut bibs = vec![0; n];
+    for i in 0..n {
+        input! {
+            q: usize,
+        }
+
+        people[i].bibs = q;
+        bibs[q - 1] = people[i].watching_person - 1;
+    }
+
+    let ans = bibs.iter().map(|bib| people[*bib].bibs).join(" ");
+
+    println!("{}", ans);
 }
