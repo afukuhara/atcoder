78451d372ac7d09a56099332119c4eeee402abc4
福原有信 <afukuhara@abc.com>
2025-03-08 15:33:37 +0900

    add my code for problem D

--- abc391/src/bin/d.rs
+++ abc391/src/bin/d.rs
@@ -17,10 +17,79 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, w: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut grid = vec![vec![0; n + 1]; w];
+    let mut existing_blocks = HashSet::new();
+    let mut offsets = vec![0; w];
+
+    for i in 0..n {
+        input! {
+            x: usize, y: usize,
+        }
+
+        grid[x - 1][y - 1] = i + 1;
+        existing_blocks.insert(i + 1);
+    }
+
+    // _show_grid(&grid, 0, &offsets);
+
+    input! {
+        q: usize,
+    }
+
+    let mut current_time = 0;
+
+    for _ in 0..q {
+        input! {
+            time: usize, a: usize,
+        }
+
+        while current_time < time {
+            // move blocks
+            let mut is_all = true;
+            let mut will_be_removed = HashSet::new();
+            for x in 0..w {
+                let current_offset = offsets[x];
+
+                let value = grid[x][current_offset];
+                if value == 0 {
+                    offsets[x] += 1;
+                    is_all = false;
+                } else {
+                    will_be_removed.insert(value);
+                }
+            }
+
+            if is_all {
+                will_be_removed.iter().for_each(|value| {
+                    existing_blocks.remove(value);
+                });
+                offsets.iter_mut().for_each(|offset| *offset += 1);
+            }
+            current_time += 1;
+        }
+
+        // _show_grid(&grid, current_time, &offsets);
+
+        println!(
+            "{}",
+            if existing_blocks.contains(&a) {
+                "Yes"
+            } else {
+                "No"
+            }
+        );
+    }
+
+    // _show_grid(&grid, current_time, &offsets);
+}
+
+fn _show_grid(grid: &[Vec<usize>], current_time: usize, offsets: &[usize]) {
+    println!("TIME: {}", current_time);
+    for (i, row) in grid.iter().enumerate() {
+        let offset = offsets[i];
+        println!("{:?}", row[offset..].to_vec());
+    }
 }
