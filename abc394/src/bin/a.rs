f01100e7661aef3828bc80fa08093dfe08b81b38
福原有信 <afukuhara@abc.com>
2025-03-21 16:33:57 +0900

    finish problem A

--- abc394/src/bin/a.rs
+++ abc394/src/bin/a.rs
@@ -1,26 +1,11 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        s: String,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let answer = s.chars().filter(|&c| c == '2').collect::<String>();
+    println!("{}", answer);
 }
