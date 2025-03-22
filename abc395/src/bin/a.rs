14ce436dea6cbbce0b31f31c2d3e81c60988862c
福原有信 <afukuhara@abc.com>
2025-03-22 15:41:46 +0900

    finish problem A

--- abc395/src/bin/a.rs
+++ abc395/src/bin/a.rs
@@ -1,26 +1,17 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let answer = if a.windows(2).any(|a| a[0] >= a[1]) {
+        "No"
+    } else {
+        "Yes"
+    };
+
+    println!("{}", answer);
 }
