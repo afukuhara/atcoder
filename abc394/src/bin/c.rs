f905713fac5089899ddb503b6d61cb3dec12a5de
福原有信 <afukuhara@abc.com>
2025-03-22 15:00:15 +0900

    finish problem C

--- abc394/src/bin/c.rs
+++ abc394/src/bin/c.rs
@@ -1,26 +1,27 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        s: String,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut chars = s.chars().collect::<Vec<_>>();
+    let mut charge_w = 0;
+
+    for i in 0..chars.len() {
+        if chars[i] == 'W' {
+            charge_w += 1;
+        } else if chars[i] == 'A' && charge_w > 0 {
+            chars[i - charge_w] = 'A';
+            for j in 0..charge_w {
+                chars[i - j] = 'C';
+            }
+            charge_w = 0;
+        } else {
+            charge_w = 0;
+        }
+    }
+
+    println!("{}", chars.iter().collect::<String>());
 }
