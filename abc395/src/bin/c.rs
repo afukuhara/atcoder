26b8ecb7cb67deb89b52f55c01e6329272a1507a
福原有信 <afukuhara@abc.com>
2025-03-23 15:10:07 +0900

    finish problem C

--- abc395/src/bin/c.rs
+++ abc395/src/bin/c.rs
@@ -1,26 +1,26 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        nums: [isize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut hash: HashMap<isize, Vec<isize>> = HashMap::new();
+
+    for (i, &num) in nums.iter().enumerate() {
+        hash.entry(num).or_default().push(i as isize);
+    }
+
+    let ans: isize = hash
+        .iter()
+        .filter(|(_, v)| v.len() > 1)
+        .flat_map(|(_, v)| v.windows(2))
+        .map(|w| w[1] - w[0] + 1)
+        .min()
+        .unwrap_or(-1);
+
+    println!("{}", ans);
 }
