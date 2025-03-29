acd88a231322ef068aac3d9918411d6319be20df
福原有信 <afukuhara@abc.com>
2025-03-29 12:05:00 +0900

    finish problem A

--- abc396/src/bin/a.rs
+++ abc396/src/bin/a.rs
@@ -1,26 +1,15 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        nums: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    if nums.windows(3).any(|w| w[0] == w[1] && w[1] == w[2]) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
