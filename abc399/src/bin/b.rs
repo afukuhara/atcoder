a9bc7979e3578f23793f66eb7ee3755a5acff8cf
福原有信 <afukuhara@abc.com>
2025-05-04 17:43:28 +0900

    finish problem B

--- abc399/src/bin/b.rs
+++ abc399/src/bin/b.rs
@@ -1,26 +1,31 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use itertools::Itertools;
+use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        p: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut number_of_points = HashMap::new();
+    p.iter().for_each(|p| {
+        number_of_points.entry(p).or_insert(0);
+        number_of_points.entry(p).and_modify(|e| *e += 1);
+    });
+
+    let mut current_rank = 1;
+    let mut rank_with_count = HashMap::new();
+    for (point, rank_count) in number_of_points.iter_mut().sorted_by_key(|(p, _)| *p).rev() {
+        rank_with_count.entry(point).or_insert(0);
+        rank_with_count
+            .entry(point)
+            .and_modify(|e| *e += current_rank);
+        current_rank += *rank_count;
+    }
+
+    for point in p.iter() {
+        println!("{}", rank_with_count[&point]);
+    }
 }
