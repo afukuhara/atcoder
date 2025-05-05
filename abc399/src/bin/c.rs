f975c97382a8a2e16318e56622ab9cb851937a07
福原有信 <afukuhara@abc.com>
2025-05-05 15:34:36 +0900

    finished C. 解説などを見て完了

--- abc399/src/bin/c.rs
+++ abc399/src/bin/c.rs
@@ -1,26 +1,45 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, m: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut graph = vec![vec![]; n];
+    for _ in 0..m {
+        input! {
+            mut a: usize, mut b: usize,
+        };
+        (a, b) = (a - 1, b - 1);
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let num_of_connected_components = count_connected_components(&graph);
+    println!("{}", m - (n - num_of_connected_components));
+}
+
+fn count_connected_components(adj: &[Vec<usize>]) -> usize {
+    let n = adj.len();
+    let mut visited = vec![false; n];
+    let mut count = 0;
+
+    for i in 0..n {
+        if !visited[i] {
+            dfs(i, adj, &mut visited);
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn dfs(v: usize, adj: &[Vec<usize>], visited: &mut [bool]) {
+    visited[v] = true;
+    for &to in &adj[v] {
+        if !visited[to] {
+            dfs(to, adj, visited);
+        }
+    }
 }
