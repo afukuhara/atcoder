ef7a38cd0c32321b65a440b015db3acce26efcc9
福原有信 <afukuhara@abc.com>
2025-05-25 15:28:54 +0900

    finish problem A

--- abc401/src/bin/a.rs
+++ abc401/src/bin/a.rs
@@ -1,26 +1,20 @@
 #[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
 use proconio::{
     fastout, input,
-    marker::{Chars, Usize1},
 };
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        s: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    println!(
+        "{}",
+        if (200..300).contains(&s) {
+            "Success"
+        } else {
+            "Failure"
+        }
+    );
 }
