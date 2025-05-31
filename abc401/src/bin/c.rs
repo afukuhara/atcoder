67ff19d2a512aa61b37a5d70986ef9b3cce97d48
福原有信 <afukuhara@abc.com>
2025-05-31 16:42:32 +0900

    解説を参考に完了

--- abc401/src/bin/c.rs
+++ abc401/src/bin/c.rs
@@ -1,26 +1,21 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
+    const MOD: i128 = 1_000_000_000;
+
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, k: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut a = vec![1; n + 1];
+    let mut s = k as i128;
+
+    for i in k..n + 1 {
+        a[i] = s;
+        s -= a[i - k];
+        s += a[i];
+        s = ((s % MOD) + MOD) % MOD; // “正の mod” に正規化
+    }
+    println!("{}", a[n]);
 }
