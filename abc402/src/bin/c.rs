67c68fc60f84165eca0abbadb69a07ea902cbc41
福原有信 <afukuhara@abc.com>
2025-06-08 17:32:51 +0900

    finish problem C

--- abc402/src/bin/c.rs
+++ abc402/src/bin/c.rs
@@ -1,26 +1,42 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, m: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut menu_list = Vec::new();
+    let mut ingigredient_list = vec![HashSet::new(); n + 1];
+
+    for i in 0..m {
+        input! { k: usize }
+
+        let mut menu = HashSet::new();
+        for _ in 0..k {
+            input! { a: usize }
+
+            menu.insert(a);
+            ingigredient_list[a].insert(i + 1);
+        }
+
+        menu_list.push(menu);
+    }
+
+    let mut ans = 0;
+    for _ in 0..n {
+        input! { b: usize }
+
+        for menu_num in ingigredient_list[b].iter() {
+            let menu_num = *menu_num - 1;
+            menu_list[menu_num].remove(&b);
+
+            if menu_list[menu_num].is_empty() {
+                ans += 1;
+            }
+        }
+
+        println!("{}", ans);
+    }
 }
