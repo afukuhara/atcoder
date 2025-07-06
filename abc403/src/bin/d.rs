b284a81192034e8e441bab39d9a84dcdbc38ccfb
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-07-06 15:45:18 +0900

    finish problem D

--- abc403/src/bin/d.rs
+++ abc403/src/bin/d.rs
@@ -1,26 +1,53 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::cmp::max;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, d: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    const INITIAL_MAX_NUM: usize = 1000005;
+
+    let mut nums = vec![0; INITIAL_MAX_NUM];
+    let mut max_num = 0;
+    for _ in 0..n {
+        input! {
+            a: usize,
+        };
+
+        nums[a] += 1;
+        max_num = max(max_num, a);
+    }
+
+    let mut ans = 0;
+
+    if d == 0 {
+        let kinds = nums.iter().filter(|&x| *x > 0).count();
+        ans = n - kinds;
+    } else {
+        for si in 0..d {
+            let mut numbers = vec![];
+            for i in (si..=max_num).step_by(d) {
+                numbers.push(nums[i]);
+            }
+
+            ans += solve(&numbers);
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn solve(nums: &[usize]) -> usize {
+    let n = nums.len();
+    let mut dp = vec![vec![0; 2]; n + 1];
+
+    for i in 0..n {
+        dp[i + 1][1] = max(dp[i][0], dp[i][1]);
+        dp[i + 1][0] = dp[i][1] + nums[i];
+    }
+
+    let mx = max(dp[n][0], dp[n][1]);
+    nums.iter().sum::<usize>() - mx
 }
