92e17795ac3aaadbe887a8388d51e87d052f3deb
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-07-26 16:40:25 +0900

    Finish problem C

--- abc405/src/bin/c.rs
+++ abc405/src/bin/c.rs
@@ -1,26 +1,19 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        nums: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut ans = 0usize;
+    let mut suffix_sum = nums.iter().sum::<usize>();
+
+    for &x in &nums {
+        suffix_sum -= x;
+        ans += x * suffix_sum;
+    }
+
+    println!("{}", ans);
 }
