c7906ffa2588dcef77167bfa35b230a6dc12fdf3
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-07-27 15:22:09 +0900

    解説を参考にして問題D完了

--- abc405/src/bin/d.rs
+++ abc405/src/bin/d.rs
@@ -1,26 +1,51 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
     input! {
         h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut matrix = vec![];
+    let mut queue = VecDeque::new();
+    for i in 0..h {
+        input! { s: String };
+
+        let row = s.chars().collect::<Vec<char>>();
+        for (j, &c) in row.iter().enumerate() {
+            if c == 'E' {
+                queue.push_back((i as isize, j as isize));
+            }
+        }
+        matrix.push(row);
+    }
+
+    let dx: Vec<isize> = vec![1, 0, -1, 0];
+    let dy: Vec<isize> = vec![0, 1, 0, -1];
+    let direction_marks = ['^', '<', 'v', '>'];
+    let (h_i, w_i) = (h as isize, w as isize);
+
+    while let Some((x, y)) = queue.pop_front() {
+        for i in 0..4 {
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+
+            if nx < 0 || nx >= h_i || ny < 0 || ny >= w_i {
+                continue;
+            }
+
+            let (ux, uy) = (nx as usize, ny as usize);
+
+            if matrix[ux][uy] != '.' {
+                continue;
+            }
+            matrix[ux][uy] = direction_marks[i];
+            queue.push_back((nx, ny));
+        }
+    }
+
+    for row in matrix {
+        println!("{}", row.iter().collect::<String>());
+    }
 }
