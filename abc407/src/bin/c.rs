7cb499947a1100d7e255bca9716d072992d41280
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-08-24 20:14:46 +0900

    finish problem C

--- abc407/src/bin/c.rs
+++ abc407/src/bin/c.rs
@@ -1,26 +1,55 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        mut s: String
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut number_of_a = s
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+
+    let mut total_count = 0;
+    let mut b_count = 0;
+
+    while let Some(first_digit) = number_of_a.pop() {
+        let digit_after_click = ((10 + first_digit) - (b_count % 10)) % 10;
+        b_count += digit_after_click;
+        total_count += digit_after_click + 1;
+    }
+
+    println!("{}", total_count);
 }
+
+/*
+S: 21
+
+#1, A -> 0
+#2, B -> 1
+#3, A -> 10
+#4, B -> 21
+
+S: 407
+
+#-0, A -> 407
+#-1, A -> 396
+#-2, A -> 285
+#-3, A -> 174
+#-4, A -> 063
+#-5, A -> 952
+#-6, A -> 841
+#-7, A -> 730
+#-8, B -> 73
+#-9, A -> 62
+#-10, B -> 51
+#-11, A -> 40
+#-12, B -> 4
+#-13, A -> 3
+#-14, A -> 2
+#-15, A -> 1
+#-16, A -> 0
+#-17, A ->
+
+*/
