9af611b5d6e539e794d23a73fc97e480685a04f4
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-09-21 17:00:56 +0900

    ABC409 問題B: 解説を見て回答

--- abc409/src/bin/b.rs
+++ abc409/src/bin/b.rs
@@ -1,26 +1,21 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        mut a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut ans = 0;
+    a.sort_unstable_by(|x, y| y.cmp(x));
+
+    for i in (1..=n).rev() {
+        if a[i - 1] >= i {
+            ans = i;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
