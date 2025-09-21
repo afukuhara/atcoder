5c1143ff47adf76f59a5c97bfe4f924e8b914a0e
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-09-21 17:33:05 +0900

    Finish problem C

--- abc409/src/bin/c.rs
+++ abc409/src/bin/c.rs
@@ -1,26 +1,31 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, l: usize,
+        d: [usize; n - 1],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    if l % 3 != 0 {
+        println!("0");
+        return;
+    }
+
+    let mut points = vec![0_u128; l];
+    points[0] = 1;
+
+    let mut position = 0;
+    for i in d {
+        position = (position + i) % l;
+        points[position] += 1;
+    }
+
+    let step = l / 3;
+    let mut count = 0;
+    for i in 0..step {
+        count += points[i] * points[i + step] * points[i + 2 * step];
+    }
+
+    println!("{}", count);
 }
