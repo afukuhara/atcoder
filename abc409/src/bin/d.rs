6cd75498f66ef86c7f04da4baa89ca28c289c5d1
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-09-22 16:11:51 +0900

    解説を見て問題Dを完了

--- abc409/src/bin/d.rs
+++ abc409/src/bin/d.rs
@@ -1,26 +1,52 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        t: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    for _ in 0..t {
+        input! {
+            n: usize,
+            s: String,
+        };
+
+        solve(&s.chars().collect::<Vec<_>>(), n);
+    }
+}
+
+fn solve(st: &[char], n: usize) {
+    let mut l = usize::MAX;
+    for i in 0..n - 1 {
+        if st[i] > st[i + 1] {
+            l = i;
+            break;
+        }
+    }
+
+    if l == usize::MAX {
+        println!("{}", st.iter().collect::<String>());
+        return;
+    }
+
+    let mut r = n;
+    for i in l + 1..r {
+        if st[l] < st[i] {
+            r = i;
+            break;
+        }
+    }
+
+    // println!("l: {}, r: {}", l, r);
+    println!(
+        "{}",
+        [
+            st[..l].iter().collect::<String>(),
+            st[l + 1..r].iter().collect::<String>(),
+            st[l].to_string(),
+            st[r..].iter().collect::<String>(),
+        ]
+        .join("")
+    );
 }
