ebe6b6615978a9d426e4d5a1cc203ba673abcece
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-09-23 15:53:21 +0900

    Finish problem C

--- abc410/src/bin/c.rs
+++ abc410/src/bin/c.rs
@@ -1,26 +1,31 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, q: usize,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut array = (1..=n).collect::<Vec<usize>>();
+    let mut offset = 0;
+
+    for _ in 0..q {
+        input! { t: usize };
+
+        match t {
+            1 => {
+                input! { p: usize, x: usize };
+                array[(p - 1 + offset) % n] = x;
+            }
+            2 => {
+                input! { p: usize };
+                println!("{}", array[(p - 1 + offset) % n]);
+            }
+            3 => {
+                input! { k: usize };
+                offset = (offset + k) % n;
+            }
+            _ => unreachable!(),
+        }
+    }
 }
