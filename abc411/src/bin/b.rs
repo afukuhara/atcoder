b2768b89265be7466d7b025e170056445a631503
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-10-05 15:30:06 +0900

    finish problem B

--- abc411/src/bin/b.rs
+++ abc411/src/bin/b.rs
@@ -1,26 +1,23 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        d: [usize; n-1],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut i = 0;
+    while n - i > 1 {
+        let mut distances = vec![];
+        let mut current = 0;
+        for d in d.iter().skip(i) {
+            current += d;
+            distances.push(current);
+        }
+
+        i += 1;
+        println!("{}", distances.iter().join(" "));
+    }
 }
