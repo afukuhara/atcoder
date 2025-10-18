47225e11b8897c17ae002a22726d5db9dc801331
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-10-18 16:40:39 +0900

    finish problem C

--- abc412/src/bin/c.rs
+++ abc412/src/bin/c.rs
@@ -1,26 +1,33 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::collections::BTreeSet;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        t: usize
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    for _ in 0..t {
+        input! {
+            n: usize, case: [usize; n]
+        }
+
+        let mut current_weight = case[0];
+        let end = case[n - 1];
+
+        let dominos = case[1..n - 1].iter().collect::<BTreeSet<_>>();
+
+        let mut count = 2;
+        while current_weight * 2 < end {
+            if let Some(next_weight) = dominos.range(current_weight + 1..=current_weight * 2).max()
+            {
+                current_weight = **next_weight;
+                count += 1;
+            } else {
+                count = -1;
+                break;
+            }
+        }
+        println!("{}", count);
+    }
 }
