5d228c108476c0d5f78923f6a2d14ce8fbec0616
Arinobu FUKUHARA <afukuhara@gmail.com>
2025-11-08 15:52:35 +0900

    解説を見てDを完了

--- abc414/src/bin/d.rs
+++ abc414/src/bin/d.rs
@@ -1,26 +1,20 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use itertools::Itertools;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, m: usize,
+        mut x: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    x.sort();
+    let total = x
+        .windows(2)
+        .map(|w| w[1] - w[0])
+        .sorted()
+        .take(n - m)
+        .sum::<usize>();
+
+    println!("{}", total);
 }
