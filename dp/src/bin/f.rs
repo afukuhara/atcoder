73729d46bcbd7bc70952b398c695002615e976ab
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-01-17 18:31:42 +0900

    finish problem F

--- dp/src/bin/f.rs
+++ dp/src/bin/f.rs
@@ -1,26 +1,44 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::cmp::max;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        s: String, t: String,
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let s = s.chars().collect::<Vec<char>>();
+    let t = t.chars().collect::<Vec<char>>();
+
+    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+    for i in 0..s.len() {
+        for j in 0..t.len() {
+            if s[i] == t[j] {
+                dp[i + 1][j + 1] = dp[i][j] + 1;
+            }
+
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i + 1][j]);
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j + 1]);
+        }
+    }
+
+    // 復元
+    let mut ans = vec![];
+    let mut i = s.len();
+    let mut j = t.len();
+
+    while i > 0 && j > 0 {
+        if dp[i][j] == dp[i - 1][j] {
+            i -= 1;
+        } else if dp[i][j] == dp[i][j - 1] {
+            j -= 1;
+        } else {
+            ans.push(s[i - 1]);
+            i -= 1;
+            j -= 1;
+        }
+    }
+
+    println!("{}", ans.iter().rev().collect::<String>());
 }
+a
