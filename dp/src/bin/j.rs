231ad1ae22d8fc2510bd98be8f3cdb20025a096f
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-02-01 15:39:02 +0900

    解説を見て (写して) Iを完了。(まだ、あまりよくわかっていない

--- dp/src/bin/j.rs
+++ dp/src/bin/j.rs
@@ -1,26 +1,53 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut dp = vec![vec![vec![-1.0; n + 1]; n + 1]; n + 1];
+    let mut one = 0;
+    let mut two = 0;
+    let mut three = 0;
+
+    for i in a {
+        match i {
+            1 => one += 1,
+            2 => two += 1,
+            3 => three += 1,
+            _ => unreachable!(),
+        }
+    }
+
+    let ans = rec(one, two, three, &mut dp, n);
+    println!("{}", ans);
+}
+
+fn rec(i: usize, j: usize, k: usize, dp: &mut [Vec<Vec<f64>>], len: usize) -> f64 {
+    if dp[i][j][k] >= 0.0 {
+        return dp[i][j][k];
+    }
+
+    if i == 0 && j == 0 && k == 0 {
+        return 0.0;
+    }
+
+    let mut res = 0.0;
+    if i > 0 {
+        res += rec(i - 1, j, k, dp, len) * i as f64;
+    }
+    if j > 0 {
+        res += rec(i + 1, j - 1, k, dp, len) * j as f64;
+    }
+    if k > 0 {
+        res += rec(i, j + 1, k - 1, dp, len) * k as f64;
+    }
+
+    res += len as f64;
+    res *= 1.0 / (i + j + k) as f64;
+
+    dp[i][j][k] = res;
+    res
 }
