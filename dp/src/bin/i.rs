2775eca55548ee29b4da03b77ca1265e47d7d581
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-02-01 15:38:09 +0900

    finish problem H and I

--- dp/src/bin/i.rs
+++ dp/src/bin/i.rs
@@ -1,26 +1,33 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        mut probs: [f64; n]
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let max_back = (n / 2) + 1;
+    let initial_face = probs[0];
+    let initial_back = 1.0 - probs[0];
+
+    let mut dp = vec![vec![0.0; max_back]; n];
+    dp[0][0] = initial_face;
+
+    if max_back > 1 {
+        dp[0][1] = initial_back;
+    }
+
+    for i in 1..n {
+        let face = probs[i];
+        let back = 1.0 - probs[i];
+
+        dp[i][0] = dp[i - 1][0] * face;
+        for j in 1..max_back {
+            dp[i][j] = dp[i - 1][j - 1] * back + dp[i - 1][j] * face;
+        }
+    }
+
+    println!("{}", dp[n - 1].iter().sum::<f64>());
 }
+b t
