16529763962f7722bfd60e25f7f226d4e42bced1
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-02-11 12:32:38 +0900

    解説を見て問題K完了

--- dp/src/bin/k.rs
+++ dp/src/bin/k.rs
@@ -1,26 +1,21 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, k: usize,
+        a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut dp = vec![false; k + 1];
+
+    for i in 1..=k {
+        for j in 0..n {
+            if (i as isize - a[j] as isize) >= 0 {
+                dp[i] |= !dp[i - a[j]];
+            }
+        }
+    }
+
+    println!("{}", if dp[k] { "First" } else { "Second" });
 }
