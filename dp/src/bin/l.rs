4c9c1f376d6990e564ad999c81434c3f465f3fbc
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-02-22 15:56:59 +0900

    finish problem L

--- dp/src/bin/l.rs
+++ dp/src/bin/l.rs
@@ -17,10 +17,31 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        a: [i128; n]
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut dp = vec![vec![-i128::MAX; n + 1]; n + 1];
+    let ans = dfs(0, n, &a, &mut dp);
+    // println!("{:?} {:?} {:?}", a, dp, ans);
+    println!("{}", ans);
+}
+
+fn dfs(l: usize, r: usize, a: &[i128], dp: &mut Vec<Vec<i128>>) -> i128 {
+    if dp[l][r] != -i128::MAX {
+        return dp[l][r];
+    }
+
+    if l == r {
+        return 0;
+    }
+
+    let mut res = -i128::MAX;
+    res = max(res, -dfs(l + 1, r, a, dp) + a[l]);
+    res = max(res, -dfs(l, r - 1, a, dp) + a[r - 1]);
+
+    dp[l][r] = res;
+
+    // println!("l: {}, r: {}, dp: {:?}", l, r, dp);
+    res
 }
