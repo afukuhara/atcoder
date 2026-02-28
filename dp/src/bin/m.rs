cd406fe0801ca5ec2187fc903c948e1d4798986d
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-02-28 16:30:43 +0900

    解答例を参考に問題Mを完了

--- dp/src/bin/m.rs
+++ dp/src/bin/m.rs
@@ -17,10 +17,28 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, k: usize,
+        a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    const MOD: i64 = 1_000_000_007;
+    let mut dp = vec![vec![0i64; k + 1]; n + 1];
+    dp[0][0] = 1;
+
+    for i in 0..n {
+        // dp[i] の累積和を求める
+        let mut sum_dp = vec![0i64; k + 2];
+        for j in 0..=k {
+            sum_dp[j + 1] = (sum_dp[j] + dp[i][j]) % MOD;
+        }
+
+        // dp[i+1] を求める
+        for j in 0..=k {
+            let left = j - min(j, a[i]);
+            let val = sum_dp[j + 1] - sum_dp[left];
+            dp[i + 1][j] = (val % MOD + MOD) % MOD; // 負対策
+        }
+    }
+
+    println!("{}", dp[n][k] % MOD);
 }
