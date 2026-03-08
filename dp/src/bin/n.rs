fe5cabb3b2c3d0552ea5d873b944f100a19c7094
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-03-08 16:33:16 +0900

    生成AIの助けを借りて、問題N完了

--- dp/src/bin/n.rs
+++ dp/src/bin/n.rs
@@ -1,26 +1,36 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+use std::cmp::min;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        slimes: [u64; n]
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut sum_dp = vec![0; n + 1];
+    for i in 0..n {
+        sum_dp[i + 1] = sum_dp[i] + slimes[i];
+    }
+
+    let mut dp = vec![vec![u64::MAX; n + 1]; n + 1];
+    for i in 0..n {
+        dp[i][i] = 0;
+    }
+
+    let ans = rec(0, n - 1, &mut dp, &sum_dp);
+    println!("{}", ans);
+}
+
+fn rec(l: usize, r: usize, dp: &mut Vec<Vec<u64>>, sum_dp: &[u64]) -> u64 {
+    if dp[l][r] != u64::MAX {
+        return dp[l][r];
+    }
+
+    for m in l..r {
+        let cost = rec(l, m, dp, sum_dp) + rec(m + 1, r, dp, sum_dp) + (sum_dp[r + 1] - sum_dp[l]);
+        dp[l][r] = min(dp[l][r], cost);
+    }
+
+    dp[l][r]
 }
