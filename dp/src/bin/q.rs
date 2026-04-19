e1c59f30085f20d63bb1b8113207983e56044859
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-04-19 16:32:17 +0900

    ChatGPTにアドバイスしてもらって、なんとか完了

--- dp/src/bin/q.rs
+++ dp/src/bin/q.rs
@@ -1,26 +1,67 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input, marker::Usize1};
+use std::cmp::max;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize,
+        h: [Usize1; n],
+        a: [usize; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut seg_tree = SegTree::new(n);
+
+    for i in 0..n {
+        let val = seg_tree.query(0, h[i]);
+        let new_val = val + a[i] as i64;
+        seg_tree.update(h[i], new_val);
+    }
+
+    println!("{}", seg_tree.query(0, n));
+}
+
+struct SegTree {
+    n: usize,
+    data: Vec<i64>,
+}
+
+impl SegTree {
+    fn new(size: usize) -> Self {
+        let mut n = 1;
+        while n < size {
+            n *= 2;
+        }
+        Self {
+            n,
+            data: vec![0; 2 * n],
+        }
+    }
+
+    fn update(&mut self, mut i: usize, x: i64) {
+        i += self.n;
+        self.data[i] = x;
+        while i > 1 {
+            i /= 2;
+            self.data[i] = max(self.data[i * 2], self.data[i * 2 + 1]);
+        }
+    }
+
+    fn query(&self, mut l: usize, mut r: usize) -> i64 {
+        let mut res = 0;
+        l += self.n;
+        r += self.n;
+        while l < r {
+            if l & 1 == 1 {
+                res = max(res, self.data[l]);
+                l += 1;
+            }
+            if r & 1 == 1 {
+                r -= 1;
+                res = max(res, self.data[r]);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        res
+    }
 }
