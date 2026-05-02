6e0d02925f8968ffedd60550587ab2c6a12e2e02
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-05-02 17:04:49 +0900

    ChatGPT にアドバイスしてもらって完了

--- dp/src/bin/s.rs
+++ dp/src/bin/s.rs
@@ -1,26 +1,41 @@
-#[allow(unused_imports)]
-use itertools::{iproduct, Itertools};
-#[allow(unused_imports)]
-use num_traits::pow;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Chars, Usize1},
-};
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
+use proconio::{fastout, input};
+
+const MOD: usize = 1_000_000_007;
 
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        k: String, d: usize
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let str_len = k.len();
+    let mut dp = vec![vec![vec![0; 2]; d]; str_len + 1];
+    dp[0][0][1] = 1;
+
+    for (i, c) in k
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .enumerate()
+    {
+        for r in 0..d {
+            for tight in 0..2 {
+                let cur = dp[i][r][tight];
+                if cur == 0 {
+                    continue;
+                }
+
+                let max_digit = if tight == 1 { c } else { 9 };
+
+                for x in 0..=max_digit {
+                    let new_r = (r + x) % d;
+                    let new_tight = (tight == 1 && x == c) as usize;
+                    dp[i + 1][new_r][new_tight] += cur;
+                    dp[i + 1][new_r][new_tight] %= MOD;
+                }
+            }
+        }
+    }
+
+    let ans = (dp[str_len][0][0] + dp[str_len][0][1] + MOD - 1) % MOD;
+    println!("{}", ans);
 }
