99d2aaa063c93b7032a8bdfe1354bce261fb2646
Arinobu FUKUHARA <afukuhara@gmail.com>
2026-07-18 16:56:18 +0900

    いったん書いてみたが、明らかに TLE になる実装なので、この後、高速化する

--- dp/src/bin/z.rs
+++ dp/src/bin/z.rs
@@ -17,10 +17,29 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+        n: usize, c: u64,
+        h: [u64; n],
     };
 
-    println!("{:?} {:?} {:?} {:?}", h, w, s, plan);
+    let mut dp = vec![u64::MAX; n];
+    dp[0] = 0;
+
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            let cost = (h[j] - h[i]).pow(2) + c;
+            dp[j] = min(dp[j], dp[i] + cost);
+
+            // println!(
+            //     "i: {}, j: {}, h[i]: {}, h[j]: {}, cost: {}, dp[j]: {}, dp[i]: {}",
+            //     i, j, h[i], h[j], cost, dp[j], dp[i]
+            // );
+        }
+
+        // println!("---------------");
+        // println!("dp: {:?}", dp);
+        // println!("--------------------------------");
+    }
+
+    // println!("{:?}", dp);
+    println!("{}", dp[n - 1]);
 }
